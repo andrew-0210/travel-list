cb3a98a60c75d060ac1b268af5a3188dda2bed50
Andrew Monthero <andrewmonthero21@gmail.com>
2024-07-13 21:26:40 +0530

    Initial commit

---
+++ src/app/fonts.js
@@ -0,0 +1,5 @@
+import { Quicksand, Monoton } from 'next/font/google';
+
+export const quicksand = Quicksand({ subsets: ['latin'] });
+
+export const monoton = Monoton({ weight: '400', subsets: ['latin'] });
--- src/app/page.js
+++ src/app/page.js
@@ -1,113 +1,147 @@
-import Image from "next/image";
+'use client';
+
+import { useState } from 'react';
+import { monoton } from './fonts';
+
+// const initialItems = [
+//   { id: 1, description: 'Passports', quantity: 2, packed: true },
+//   { id: 2, description: 'Socks', quantity: 12, packed: false },
+// ];
 
 export default function Home() {
+  const [items, setItems] = useState([]);
+
+  const handleAddItems = item => {
+    setItems(items => [...items, item]);
+  };
+
+  const handleDeleteItem = itemId => {
+    setItems(items => items.filter(item => item.id !== itemId));
+  };
+
+  const handleToggleItem = itemId => {
+    setItems(items =>
+      items.map(item =>
+        item.id === itemId ? { ...item, packed: !item.packed } : item
+      )
+    );
+  };
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    <>
+      <Logo />
+      <Form onAddItems={handleAddItems} />
+      <PackagingList
+        items={items}
+        onDeleteItem={handleDeleteItem}
+        onToggleItem={handleToggleItem}
+      />
+      <Stats />
+    </>
   );
 }
+
+const Logo = () => {
+  return (
+    <h1
+      className={`${monoton.className} text-center bg-[#f4a226] text-[8rem] font-[400] uppercase px-[2.4rem] -tracking-[5px]`}
+    >
+      Far Away
+    </h1>
+  );
+};
+
+const Form = ({ onAddItems }) => {
+  const [quantity, setQuantity] = useState(1);
+  const [description, setDescription] = useState('');
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    if (!description) return;
+
+    const newItem = { description, quantity, packed: false, id: Date.now() };
+
+    onAddItems(newItem);
+    setQuantity(1);
+    setDescription('');
+  };
+
+  return (
+    <form
+      className='bg-[#e5771f] py-[2.8rem] flex items-center justify-center gap-[0.8rem]'
+      onSubmit={handleSubmit}
+    >
+      <h3 className='mr-[1.6rem] text-[2.rem] font-semibold'>
+        What do you need for your trip?
+      </h3>
+
+      <select value={quantity} onChange={e => setQuantity(+e.target.value)}>
+        {Array.from({ length: 20 }, (_, i) => i + 1).map(num => {
+          return (
+            <option value={num} key={num}>
+              {num}
+            </option>
+          );
+        })}
+      </select>
+      <input
+        type='text'
+        placeholder='Item...'
+        value={description}
+        onChange={e => setDescription(e.target.value)}
+      />
+
+      <button className='bg-[#76c7ad] uppercase rounded-[10rem] px-[3.2rem] py-[1.2rem] cursor-pointer text-[1.8rem] font-[700]'>
+        Add
+      </button>
+    </form>
+  );
+};
+
+const PackagingList = ({ items, onDeleteItem, onToggleItem }) => {
+  return (
+    <div className='list'>
+      <ul>
+        {items.map(item => (
+          <Item
+            item={item}
+            key={item.id}
+            onDeleteItem={onDeleteItem}
+            onToggleItem={onToggleItem}
+          />
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+const Item = ({ item, onDeleteItem, onToggleItem }) => {
+  return (
+    <li>
+      <input
+        type='checkbox'
+        value={item.packed}
+        onChange={() => {
+          onToggleItem(item.id);
+        }}
+      />
+      <span style={item.packed ? { textDecoration: 'line-through' } : {}}>
+        {item.quantity} {item.description}
+      </span>
+      <button
+        className='cursor-pointer bg-none border-none text-[1.8rem] p-[0.8rem] translate-y-[2px]'
+        onClick={() => onDeleteItem(item.id)}
+      >
+        ❌
+      </button>
+    </li>
+  );
+};
+
+const Stats = () => {
+  return (
+    <footer className='stats'>
+      <em>You have X items on your list, and you already packed X (X%)</em>
+    </footer>
+  );
+};
